d4eadccbfc503a12ef674e09c28dc0683d6bc32d
Germain Grumel <germaingrumel@outlook.fr>
2020-11-12 17:56:31 +0100

    premier commit

--- exos/3_dom/index.js
+++ exos/3_dom/index.js
@@ -9,6 +9,16 @@
 */
 
 const colors = ['blue', 'red', 'green', 'black', 'grey', 'orange', 'purple'];
+const exercice1 = document.getElementById('exo1');
+colors.forEach ((color) => {
+ let element = document.createElement('div');
+ element.textContent = color;
+ element.style.backgroundColor = color;
+ exo1.append(element);
+ element.addEventListener('click',function(){
+     console.log(this.textContent);
+ });
+});
 
 // -------------------------------
 
@@ -21,6 +31,16 @@ const colors = ['blue', 'red', 'green', 'black', 'grey', 'orange', 'purple'];
 
 // -------------------------------
 
+const exercice2 = document.createElement('section');
+exercice1.after(exercice2);
+let div2 = document.createElement('div');
+div2.style.backgroundColor = 'black';
+exo2.append(div2);
+exo2.addEventListener('mousemove', function(e){
+    div2.style.width = e.clientX + 'px';
+    div2.style.height = e.clientY + 'px';
+})
+
 /* Exercice 3: Harry & friends
     - Créer une <section> avec l'id 'exo3', et l'ajouter au body
     - Créer une <div> pour Harry, avec le nom en textContent, et l'ajouter à la 3e section
@@ -29,6 +49,17 @@ const colors = ['blue', 'red', 'green', 'black', 'grey', 'orange', 'purple'];
 */
 
 const names = ['Harry', 'Hermione', 'Ron', 'Sirius', 'Hagrid', 'Albus'];
+const exo3 = document.createElement('section');
+exo3.id = 'exo3';
+exo2.after(exo3);
+let div3 = document.createElement('div');
+div3.textContent = names[0];
+exo3.append(div3);
+exo3.addEventListener('click', function(){
+    let rand = Math.floor(Math.random()*names.length);
+    div3.textContent = names[rand];
+});
+
 
 // -------------------------------
 
@@ -40,6 +71,35 @@ const names = ['Harry', 'Hermione', 'Ron', 'Sirius', 'Hagrid', 'Albus'];
 */
 
 // -------------------------------
+
+const exo4 = document.createElement('section');
+exo4.id = 'exo4';
+exo3.after(exo4);
+let button4 = document.createElement('button');
+button4.textContent = 'Track';
+exo4.append(button4);
+let exo4X = document.createElement('div');
+let exo4Y = document.createElement('div');
+let exo4Space = document.createElement('div');
+
+exo4.append(exo4X);
+exo4X.after(exo4Space);
+exo4.append(exo4Y);
+let exo4toggle = false;
+button4.addEventListener('click', function(){
+    exo4toggle = !exo4toggle;
+    if(exo4toggle) {
+        document.addEventListener('mousemove', exo4Move);
+    }
+    else{
+        document.removeEventListener('mousemove', exo4Move);
+    }
+});
+function exo4Move(e){
+    console.log(e.clientX, e.clientY);
+    exo4X.innerHTML = e.clientX + " " + e.clientY;
+}
+
 
 /* Exercice Bonus: Click and drag
     - Créer une <section> avec l'id 'exo5', et l'ajouter au body
